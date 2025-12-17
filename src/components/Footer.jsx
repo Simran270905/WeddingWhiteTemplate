09dae5c819996177cb3b12439c6b2c0278e7b4ed
Simran270905 <simrankadamkb12@gmail.com>
2025-12-17 18:40:51 +0530

    change

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,17 +4,17 @@ export default function Footer() {
       <div className="max-w-6xl mx-auto px-6 text-center space-y-4">
         
         <p className="text-lg">
-          📞 Contact us: <span className="text-white">+91 99999</span>
+          📞 Contact us: <span className="text-white">+91 0123456789</span>
         </p>
 
         <p className="text-sm">
-          ✉️ Email: <span className="text-white">weddinginvite@email.com</span>
+          ✉️ Email: <span className="text-white">starxinnovation@gmail.com</span>
         </p>
 
         <hr className="border-gray-600 my-4" />
 
         <p className="text-xs">
-          © {new Date().getFullYear()} Wedding Invitation. All Rights Reserved.
+          © {new Date().getFullYear()} StarX Innovations and IT Solution. All Rights Reserved.
         </p>
       </div>
     </footer>
